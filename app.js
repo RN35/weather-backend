8ab91097d3e13a05c13ebccde8ef0bd9850e32ff
Rohit <rohitnair35@gmail.com>
2022-02-04 21:08:33 -0800

    secured /v1/hello and /v1/weather with basic token comparison; not checking for expiry

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 console.log('Node. js Express server is running on port 3000... ')
 
-app.use(function (req, res, next) {
+app.use(function (request, response, next) {
     // Website you wish to allow to connect
     const allowedOrigins = ['https://editor.swagger.io', 'https://hoppscotch.io'];
-    const origin = req.headers.origin;
+    const origin = request.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
+        response.setHeader('Access-Control-Allow-Origin', origin);
     }
     // Request methods you wish to allow eg: GET, POST, OPTIONS, PUT, PATCH, DELETE
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    // response.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
@@ -35,15 +35,38 @@ app.use(function (req, res, next) {
 app.get('/v1/weather', get_weather)
 
 function get_weather(request, response) {
-    response.json({ "coord": { "lon": -123.262, "lat": 44.5646 }, "weather": [{ "id": 801, "main": "Clouds", "description": "few clouds", "icon": "02n" }], "base": "stations", "main": { "temp": 58, "feels_like": 49.98, "temp_min": 48.09, "temp_max": 55.45, "pressure": 1023, "humidity": 78 }, "visibility": 10000, "wind": { "speed": 0, "deg": 0 }, "clouds": { "all": 20 }, "dt": 1641958461, "sys": { "type": 2, "id": 2040223, "country": "US", "sunrise": 1641916079, "sunset": 1641948820 }, "timezone": -28800, "id": 5720727, "name": "Corvallis", "cod": 200 })
+    const authHeader = request.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    if (token == jwt_token) {
+        response.json({ "coord": { "lon": -123.262, "lat": 44.5646 }, "weather": [{ "id": 801, "main": "Clouds", "description": "few clouds", "icon": "02n" }], "base": "stations", "main": { "temp": 58, "feels_like": 49.98, "temp_min": 48.09, "temp_max": 55.45, "pressure": 1023, "humidity": 78 }, "visibility": 10000, "wind": { "speed": 0, "deg": 0 }, "clouds": { "all": 20 }, "dt": 1641958461, "sys": { "type": 2, "id": 2040223, "country": "US", "sunrise": 1641916079, "sunset": 1641948820 }, "timezone": -28800, "id": 5720727, "name": "Corvallis", "cod": 200 })
+    }
+    else {
+        return response.sendStatus(403)
+    }
+
 }
 
 app.get('/v1/hello', get_greeting)
 
 function get_greeting(request, response) {
-    response.json({
-        "message": "Good morning"
-    })
+    const authHeader = request.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return response.sendStatus(401)
+
+    if (token == jwt_token) {
+        response.json({
+            "message": "Good morning"
+        })
+    }
+    else {
+        return response.sendStatus(403)
+    }
+
+
 }
 
 app.post('/v1/auth', (request, response) => {
@@ -58,3 +81,5 @@ app.post('/v1/auth', (request, response) => {
 
 
 })
+
+
